54d623f5702d24fd08336e0cb92ee4e6b379d53c
hfdem <yangruipengchn@gmail.com>
2022-02-10 17:48:57 +0800

    Hw07: Q1: Thane of Cadr

---
+++ homeworks/hw07/hw07.scm
@@ -0,0 +1,11 @@
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cdr (cdr s))))
+
+(define (ordered? s) 'YOUR-CODE-HERE)
+
+(define (square x) (* x x))
+
+(define (pow base exp) 'YOUR-CODE-HERE)
