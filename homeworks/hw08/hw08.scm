4548af054c524fd11a773dc213faf891577de32f
hfdem <yangruipengchn@gmail.com>
2022-02-11 21:05:28 +0800

    Hw08: Q2: Interleave

--- homeworks/hw08/hw08.scm
+++ homeworks/hw08/hw08.scm
@@ -3,7 +3,10 @@
     ((func (car lst)) (cons (car lst) (my-filter func (cdr lst))))
     (else (my-filter func (cdr lst)))))
 
-(define (interleave s1 s2) 'YOUR-CODE-HERE)
+(define (interleave s1 s2)
+  (cond ((null? s1) s2)
+    ((null? s2) s1)
+    (else (append (list (car s1)) (list (car s2)) (interleave (cdr s1) (cdr s2))))))
 
 (define (accumulate merger start n term)
   'YOUR-CODE-HERE)
