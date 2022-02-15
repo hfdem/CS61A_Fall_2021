3a7aa312242c9611b809f5d090671785dac53eda
hfdem <yangruipengchn@gmail.com>
2022-02-16 04:52:02 +0800

    Project 4: Scheme Interpreter: Problem 9

--- projects/scheme/scheme_eval_apply.py
+++ projects/scheme/scheme_eval_apply.py
@@ -59,7 +59,7 @@ def scheme_apply(procedure, args, env):
         # END PROBLEM 2
     elif isinstance(procedure, LambdaProcedure):
         # BEGIN PROBLEM 9
-        "*** YOUR CODE HERE ***"
+        return eval_all(procedure.body, procedure.env.make_child_frame(procedure.formals, args))
         # END PROBLEM 9
     elif isinstance(procedure, MuProcedure):
         # BEGIN PROBLEM 11
