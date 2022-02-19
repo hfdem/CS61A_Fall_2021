4795ec45fbb51ed68f2883edb358d9c13bfea2d6
hfdem <yangruipengchn@gmail.com>
2022-02-19 09:13:37 +0800

    Lab14: Q2: Split

--- lab/lab14/lab14.scm
+++ lab/lab14/lab14.scm
@@ -1,3 +1,11 @@
-(define (split-at lst n) 'YOUR-CODE-HERE)
+(define (split-at lst n)
+    (define (split-car lst n)
+        (if (= n 0) nil
+            (append (list(car lst)) (split-car (cdr lst) (- n 1)))))
+    (define (split-cdr lst n)
+        (if (= n 0) lst
+            (split-cdr (cdr lst) (- n 1))))
+    (if (< n (length lst)) (append (list(split-car lst n)) (split-cdr lst n))
+        (list lst)))
 
 (define (compose-all funcs) 'YOUR-CODE-HERE)
